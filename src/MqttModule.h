ff6d768bb3cbfbcaef944d6fcab9e6a627e66e63
thefipster <felix.reisch@live.de>
2023-03-09 23:56:05 +0100

    Target temperature controlable by mqtt topic. Removed tune page. Renamed mqtt topics.

--- src/MqttModule.h
+++ src/MqttModule.h
@@ -6,10 +6,11 @@
 #include <PubSubClient.h>
 
 void setupMqtt(IPAddress ip, uint16_t port, int interval);
-void setupMqttTopics(char *boilerTopic, char *headTopic, char *pidTopic);
+void setupMqttTopics(char *boilerTopic, char *headTopic, char *pidTopic, char *heaterTopic);
 void connectMqtt(char* clientId, char* user, char* password);
 void publish(double boiler, double head, double pid);
 void publishState(bool state);
 void ensureMqtt(char *clientId, char *user, char *password);
+float getTargetTemperature();
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,12 @@
 
 #define MQTT_CLIENTID "rancilio-silvia"
 #define MQTT_USERNAME "{{mqtt_username}}"
-#define MQTT_PASSWORD "{{mqtt_passwort}}"
+#define MQTT_PASSWORD "{{mqtt_password}}"
 
-#define MQTT_PID_TOPIC "lab/pid/control"
-#define MQTT_BOILER_TOPIC "lab/pid/boiler"
-#define MQTT_HEAD_TOPIC "lab/pid/head"
+#define MQTT_PID_TOPIC "kitchen/rancilio/control"
+#define MQTT_BOILER_TOPIC "kitchen/rancilio/boiler"
+#define MQTT_HEAD_TOPIC "kitchen/rancilio/head"
+#define MQTT_HEATER_TOPIC "kitchen/rancilio/state"
 
 #define PID_WINDOW 1000
 #define PID_SETPOINT 90
@@ -52,7 +53,7 @@ void setup()
   webSetpoint = PID_SETPOINT;
   setupWifi(HOSTNAME, WIFI_SSID, WIFI_PASS);
   setupMqtt(MQTT_IP, MQTT_PORT, MQTT_INTERVAL);
-  setupMqttTopics(MQTT_BOILER_TOPIC, MQTT_HEAD_TOPIC, MQTT_PID_TOPIC);
+  setupMqttTopics(MQTT_BOILER_TOPIC, MQTT_HEAD_TOPIC, MQTT_PID_TOPIC, MQTT_HEATER_TOPIC);
   connectMqtt(MQTT_CLIENTID, MQTT_USERNAME, MQTT_PASSWORD);
   setupWebserver();
 }
@@ -69,58 +70,12 @@ void loop()
   updateLed();
   ensureWifi(WIFI_SSID, WIFI_PASS);
   ensureMqtt(MQTT_CLIENTID, MQTT_USERNAME, MQTT_PASSWORD);
+  webSetpoint = getTargetTemperature();
   setPidTuning(webKp, webKi, webKp);
   setPidParameters(webSetpoint, webColdstart, webWindow, webCycle);
 }
 
 AsyncWebServer server(80);
-const char *input_parameter1 = "kp";
-const char *input_parameter2 = "ki";
-const char *input_parameter3 = "kd";
-const char *input_parameter4 = "window";
-const char *input_parameter5 = "cycle";
-const char *input_parameter6 = "coldstart";
-const char *input_parameter7 = "setpoint";
-
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html><head>
-  <title>PID Tuning</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <style>
-    html {font-family: Verdana; display: inline-block; text-align: center;}
-    h2 {font-size: 3.0rem;}
-  </style>
-  </head><body>
-  <h2>Tuning Parameters</h2> 
-  <form action="/get">
-    kP <input type="text" name="kp">
-    <input type="submit" value="Submit">
-  </form><br>
-  <form action="/get">
-    kI: <input type="text" name="ki">
-    <input type="submit" value="Submit">
-  </form><br>
-  <form action="/get">
-    kD: <input type="text" name="kd">
-    <input type="submit" value="Submit">
-  </form><br>
-  <form action="/get">
-    Window: <input type="text" name="window">
-    <input type="submit" value="Submit">
-  </form><br>
-  <form action="/get">
-    Cycle: <input type="text" name="cycle">
-    <input type="submit" value="Submit">
-  </form><br>
-  <form action="/get">
-    Setpoint: <input type="text" name="setpoint">
-    <input type="submit" value="Submit">
-  </form><br>
-  <form action="/get">
-    Coldstart: <input type="text" name="coldstart">
-    <input type="submit" value="Submit">
-  </form>
-</body></html>)rawliteral";
 
 void notFound(AsyncWebServerRequest *request)
 {
@@ -132,42 +87,6 @@ void setupWebserver()
   server.onNotFound(notFound);
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
             { request->send(418, "text/plain", "I am a coffee machine."); });
-  server.on("/tune", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send_P(200, "text/html", index_html); });
-  server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request)
-            {
-              String message;
-              if (request->hasParam(input_parameter1)) {
-                message = request->getParam(input_parameter1)->value();
-                webKp = message.toDouble();
-              }
-              else if (request->hasParam(input_parameter2)) {
-                message = request->getParam(input_parameter2)->value();
-                webKi = message.toDouble();
-              }
-              else if (request->hasParam(input_parameter3)) {
-                message = request->getParam(input_parameter3)->value();
-                webKd = message.toDouble();
-              }
-              else if (request->hasParam(input_parameter4)) {
-                message = request->getParam(input_parameter4)->value();
-                webWindow = message.toDouble();
-              }
-              else if (request->hasParam(input_parameter5)) {
-                message = request->getParam(input_parameter5)->value();
-                webCycle = message.toDouble();
-              }
-              else if (request->hasParam(input_parameter6)) {
-                message = request->getParam(input_parameter6)->value();
-                webColdstart = message.toDouble();
-              }
-              else if (request->hasParam(input_parameter7)) {
-                message = request->getParam(input_parameter7)->value();
-                webSetpoint = message.toDouble();
-              }
-              
-              request->redirect("/tune"); });
-
   server.on("/config", HTTP_GET, [](AsyncWebServerRequest *request)
             { request->send(200, "text/html", printConfig()); });
 
